ab30594b138c0302079191efacc8134d576334a9
igonzalezcastro <igonzalezcastro@atanasoff21.danielcastelao.org>
2016-04-21 12:23:25 +0200

    Boletin27

---
+++ src/boletin27/Boletin27.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin27;
+
+/**
+ *
+ * @author igonzalezcastro
+ */
+public class Boletin27 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Datos tabla = new Datos();
+       tabla.setVisible(true);
+    }
+    
+}
